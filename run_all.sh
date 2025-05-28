2ab649782d38fd47746cb78250861000088da190
hyh <472186159@qq.com>
2025-05-28 16:28:09 +0800

    本地恢复应该没问题了

--- run_all.sh
+++ run_all.sh
@@ -1,17 +1,17 @@
-sh kill_all.sh
-rm /dev/shm/*
+# sh kill_all.sh
+# rm /dev/shm/*
 
-python shared_holder.py &
-shared_holder_process_id=$!
-sleep 1
+# python shared_holder.py &
+# shared_holder_process_id=$!
+# sleep 1
 # sh idp_opt.sh &
 # idp_opt_process_id=$!
 
 # sh see_nvidia.sh &
 # see_nvidia_process_id=$!
-sleep 1
+# sleep 1
 sh run_my_megatron_deepspeed.sh 
 # sh run_nsight_ds.sh
-sleep 1
-kill $shared_holder_process_id
+# sleep 1
+# kill $shared_holder_process_id
 # kill $idp_opt_process_id
